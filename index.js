169003da4409dec474edaada58c33aee9d61b8ab
Luis Otero <alonsooteroseminario@gmail.com>
2022-10-16 17:04:01 -0300

    added public folder

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 3000
 
